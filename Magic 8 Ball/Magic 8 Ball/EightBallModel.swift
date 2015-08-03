b23a16d6c50dadfaaf4536c2a7ed35a801b7785a
PeterMav <peter.mavo@gmail.com>
2015-08-03 13:17:42 +1000

    .

---
+++ Magic 8 Ball/Magic 8 Ball/EightBallModel.swift
@@ -0,0 +1,27 @@
+//
+//  EightBallModel.swift
+//  Magic 8 Ball
+//
+//  Created by Peter Mavridis on 3/08/2015.
+//  Copyright (c) 2015 Peter Mavridis. All rights reserved.
+//
+
+import Foundation
+
+// Default Constructor. Sets the responseArray property with
+// the initialResponseArray
+
+class EightBallModel : Printable, DebugPrintable {
+    let initialResponseArray : String = "Yes!"
+    
+}
+
+init () {
+
+    
+}
+
+init(extraResponseArray : Array<String>) {
+    
+    
+}
