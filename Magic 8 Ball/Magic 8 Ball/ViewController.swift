a6e16878d6c866c92d9785d88ced8cda56eb82f3
PeterMav <csci321ga1a@cs321s-iMac.local>
2015-08-03 13:50:08 +1000

    .

--- Magic 8 Ball/Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/Magic 8 Ball/ViewController.swift
@@ -23,9 +23,16 @@ class ViewController: UIViewController {
         var EightBall = EightBallModel()
         EightBall.responseArray += ["Yes!"]
         EightBall.responseArray += ["No!"]
+        EightBall.responseArray += ["Maybe"]
         
         println("Will I get full marks for this lab?")
+        println(EightBall.responseArray[0])
         
+        println("Will the Cronulla Sharks receive a premiership this year?")
+        println(EightBall.responseArray[1])
+        
+        println("Will I end up becoming a cat person when I get old?")
+        println(EightBall.responseArray[2])
         
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
