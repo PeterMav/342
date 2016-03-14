6ca3552d08e01507206655e29f173f8818425539
PeterMav <peter.mavo@gmail.com>
2016-03-14 13:16:55 +1100

    ..

--- Magic 8 Ball/Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/Magic 8 Ball/ViewController.swift
@@ -17,14 +17,14 @@ class ViewController: UIViewController, UITextFieldDelegate {
     
     override func viewDidLoad() {
         
-        print("Peter Mavridis")
-        
-        var age : Double = 107.392294029
-        print("My age: ")
-        print(String(format: "%.2f", age))
-        
-        let nameVariable : String = "Peter Mavridis"
-        print(nameVariable)
+//        print("Peter Mavridis")
+//        
+//        var age : Double = 107.392294029
+//        print("My age: ")
+//        print(String(format: "%.2f", age))
+//        
+//        let nameVariable : String = "Peter Mavridis"
+//        print(nameVariable)
         
 
         outputText.alpha = 0.0
@@ -37,20 +37,20 @@ class ViewController: UIViewController, UITextFieldDelegate {
         EightBall.responseArray += [" Maybe"]
         EightBall.responseArray += [" 💩"]
         */
-        print("")
-        print("Will I get full marks for this lab?\n")
-        EightBall.randomSelector()
-        
-        print("Will the Cronulla Sharks receive a premiership this year?\n")
-        EightBall.randomSelector()
-        
-        print("Will I end up becoming a cat person when I get old?\n")
-        EightBall.randomSelector()
-        
-        
-        print(EightBall.description)
-
-        debugPrint("Debug: \(EightBall.debugDescription)")
+//        print("")
+//        print("Will I get full marks for this lab?\n")
+//        EightBall.randomSelector()
+//        
+//        print("Will the Cronulla Sharks receive a premiership this year?\n")
+//        EightBall.randomSelector()
+//        
+//        print("Will I end up becoming a cat person when I get old?\n")
+//        EightBall.randomSelector()
+//        
+//        
+//        print(EightBall.description)
+//
+//        debugPrint("Debug: \(EightBall.debugDescription)")
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
