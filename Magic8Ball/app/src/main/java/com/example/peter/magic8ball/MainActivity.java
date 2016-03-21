ed0aa44305f67590527b85f601edaed2ba8a1c3e
PeterMav <peter.mavo@gmail.com>
2016-03-21 14:17:37 +1100

    Complete
    Android and iOS complete for lab 2

--- Magic8Ball/app/src/main/java/com/example/peter/magic8ball/MainActivity.java
+++ Magic8Ball/app/src/main/java/com/example/peter/magic8ball/MainActivity.java
@@ -6,8 +6,14 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.view.KeyEvent;
+import android.widget.TextView.OnEditorActionListener;
+
 
 import java.util.Random;
 
@@ -15,26 +21,62 @@ class Magic8BallModel extends Object {
     public Magic8BallModel () {
 
         responseArray = new String[]{"Heck Yes!", "Heck No!", "Maybe?"};
+        imageArray = new int[]{
+                R.drawable.circle1,
+                R.drawable.circle2,
+                R.drawable.circle3,
+                R.drawable.circle4,
+                R.drawable.circle5,
+                R.drawable.circle6
+        };
         
     }
     public String[] responseArray;
-/*
-    public void randomSelection(){
-        System.out.println(responseArray[new Random().nextInt(responseArray.length)]);
-    }
-    */
+    public int[] imageArray;
+    Random r = new Random();
+
+
+
 }
 
 
 public class MainActivity extends AppCompatActivity {
+    EditText textView3;
+    EditText textGo;
+    Button shakeGo;
     TextView textView2;
     ImageView imageView2;
-
     Magic8BallModel eightBall;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // For the shake button.
+        shakeGo = (Button) findViewById(R.id.button);
+        shakeGo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                selectRandom();
+            }
+        });
+
+
+        // For the Go button on the keyboard.
+        // Getting reference to the EditText
+        textGo = (EditText) findViewById(R.id.textView3);
+        // Setting EditorActionListener for the EditText
+        textGo.setOnEditorActionListener(new OnEditorActionListener() {
+
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if(actionId==EditorInfo.IME_ACTION_GO){
+                    selectRandom();
+                }
+                return false;
+            }
+        });
         /*
         System.out.println("Peter Mavridis");
         double age = 102.48394;
@@ -51,13 +93,16 @@ public class MainActivity extends AppCompatActivity {
         eightBall.randomSelection();
         */
         textView2 = (TextView) findViewById(R.id.textView2);
+        textView3 = (EditText) findViewById(R.id.textView3);
         imageView2 = (ImageView) findViewById(R.id.imageView2);
         eightBall = new Magic8BallModel();
-
+        textView3.setImeOptions(EditorInfo.IME_ACTION_GO);
     }
-    public void selectRandom(View view){
+    public void selectRandom(){
         textView2.setText(eightBall.responseArray[new Random().nextInt(eightBall.responseArray.length)]);
-
+        int rand = (int)(Math.random()* 5);
+        int randomInt = eightBall.r.nextInt(eightBall.imageArray.length);
+        imageView2.setBackgroundResource(eightBall.imageArray[randomInt]);
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
