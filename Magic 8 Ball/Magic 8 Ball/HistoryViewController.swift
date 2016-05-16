ebad3dc854145b64b79ebfb8b00e9f07bab5db3b
PeterMav <peter.mavo@gmail.com>
2016-05-16 14:19:01 +1000

    Complete

--- Magic 8 Ball/Magic 8 Ball/HistoryViewController.swift
+++ Magic 8 Ball/Magic 8 Ball/HistoryViewController.swift
@@ -16,15 +16,15 @@ class HistoryViewController: UIViewController, UITableViewDelegate {
     @IBOutlet weak var tableView: UITableView!
     var historyList = [QuestionResponseModel]()
     override func viewDidLoad() {
-        RemoteConnection().getHistory(){ success, data in
-            if success{
-                self.historyList = data!
-                self.tableView.reloadData()
-                print("Success")
-            } else {
-                print("Failed")
-            }
-        }
+//        RemoteConnection().getHistory(){ success, data in
+//            if success{
+//                self.historyList = data!
+//                self.tableView.reloadData()
+//                print("Success")
+//            } else {
+//                print("Failed")
+//            }
+//        }
         super.viewDidLoad()
     }
     
@@ -56,21 +56,29 @@ class HistoryViewController: UIViewController, UITableViewDelegate {
         let row = indexPath.row
         cell.answer?.text = historyList[row].answer
         cell.question?.text = historyList[row].question
-        print("In here")
+//        cell.user.image = NSURL(string: historyList[row].image!)
 //        print(historyList[row].image!)
+        
+        if let myImage = historyList[row].image {
+//            print(myImage)
+            let url = NSURL(string: historyList[row].image!)
+            let ss = NSURLSession.sharedSession()
+            ss.dataTaskWithURL(url!, completionHandler: {(data,response,error) in
+                if let e = error {
+                    print("Error \(e.localizedDescription)")
+                } else if let data = data, image = UIImage(data: data) {
+                    dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                        cell.user.image = image
+                    })
+                }
+            }).resume()
+
+        }
+        else {
+            print(historyList[row])
+        }
  
-        let url = NSURL(string: historyList[row].image!)
-        let session = NSURLSession.sharedSession()
-        session.dataTaskWithURL(url!, completionHandler: {(data,response,error) in
-            if let e = error {
-                print("Error \(e.localizedDescription)")
-            } else if let data = data, image = UIImage(data: data) {
-                dispatch_async(dispatch_get_main_queue(), { () -> Void in
-                    cell.user.image = image
-                })
-            }
-        }).resume()
-    return cell
+            return cell
     }
 
     @IBAction func doneButtonPressed(sender: AnyObject) {
